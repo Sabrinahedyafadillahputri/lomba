3e77b2d118d2c644fe6cf8d16253a4c95088bb73
sabrina hfp <sabrinahfp@gmail.com>
2021-09-13 10:16:39 +0700

    lomba

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+    $nav = $('.nav');
+    $toggleCollapse = $('.bar');
+
+    $toggleCollapse.click(function() {
+        $nav.toggleClass('barss');
+    })
+
+});
